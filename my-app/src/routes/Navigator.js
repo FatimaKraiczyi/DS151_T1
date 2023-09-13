fb2eeedef3544ecfbe507e49c21f6ff6b288ed94
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-09-13 02:54:35 -0300

    folders

---
+++ my-app/src/routes/Navigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home/Home';
+
+const Stack = createNativeStackNavigator();
+
+function Navigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Navigator
